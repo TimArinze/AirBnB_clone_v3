c327ccb2365b878812d42d198d3f35df0bc25fc5
Arinze Nwutobo <tim.arinze@outlook.com>
2023-01-23 22:30:44 +0100

    task

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ creating route
 from api.v1.views import app_views
 import json
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
@@ -13,4 +14,12 @@ def status():
     return jsonify(status="OK")
 
 
-@app_views.route('/api/v1/stats)
+@app_views.route('/stats')
+def counting():
+    """retrieve and count"""
+    return jsonify(amenities=storage.count('Amenity'),
+                   cities=storage.count('City'),
+                   places=storage.count('Place'),
+                   reviews=storage.count('Review'),
+                   states=storage.count('State'),
+                   users=storage.count('User'))
--- console.py
+++ console.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ console """
-
 import cmd
 from datetime import datetime
 import models
