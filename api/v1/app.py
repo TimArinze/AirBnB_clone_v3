a65303559582403182f69d2b1da5c5147653cfb7
Arinze Nwutobo <tim.arinze@outlook.com>
2023-01-27 00:35:56 +0100

    task

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,9 +6,11 @@ from flask import Flask, Blueprint, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
