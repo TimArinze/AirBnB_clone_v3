93a2079d04625f58b01e96abd7c73f75eb843204
Arinze Nwutobo <tim.arinze@outlook.com>
2023-05-03 00:52:23 +0100

    security

--- models/user.py
+++ models/user.py
@@ -2,10 +2,9 @@
 """ holds class User"""
 import models
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -27,3 +26,9 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+    def __setattr__(self, name, value):
+        if name == "password":
+            encoded = value.encode()
+            hashed = hashlib.md5(encoded).hexdigest()
+        super().__setattr__(name, hashed)
